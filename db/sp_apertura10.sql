d0510c575a9cde0ad5dab20966bd9e585e04a720
Ismael García <igh1@hotmail.com>
2017-07-18 14:18:41 -0500

    Procedimientos almacenados decimal 11, 3

--- db/sp_apertura10.sql
+++ db/sp_apertura10.sql
@@ -2,16 +2,16 @@ DROP PROCEDURE IF EXISTS sp_apertura10;
 DELIMITER $$
 CREATE PROCEDURE sp_apertura10(
     IN `id_benef` INT, 
-    IN `imp_cap` DECIMAL, 
-    IN `imp_eng` DECIMAL, 
+    IN `imp_cap` DECIMAL(11,3), 
+    IN `imp_eng` DECIMAL(11,3), 
     IN `poliza` VARCHAR(4), 
     IN `fecha_pol` VARCHAR(11), 
     IN `clave_b` VARCHAR(12), 
-    IN `imp_interes` DECIMAL, 
-    IN `imp_admon` DECIMAL, 
-    IN `imp_svida` DECIMAL, 
-    IN `imp_pagos` DECIMAL, 
-    IN `imp_sui` DECIMAL,
+    IN `imp_interes` DECIMAL(11,3), 
+    IN `imp_admon` DECIMAL(11,3), 
+    IN `imp_svida` DECIMAL(11,3), 
+    IN `imp_pagos` DECIMAL(11,3), 
+    IN `imp_sui` DECIMAL(11,3),
     IN `id_usuario` INT)
 BEGIN
     insert into mov_edoscta (id_benef,clave_mov,capital, poliza,fecha_pol, clave_b, id_usuario) values (id_benef,'CAP',imp_cap,poliza,fecha_pol, clave_b, id_usuario);
