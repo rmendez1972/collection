ac332bcd1abd85545315420d20e6df291320cf19
marlongomez <marlonegomezh@gmail.com>
2017-07-18 14:35:53 -0500

    se modificaron los procedimientos

--- db/sp_apertura8.sql
+++ db/sp_apertura8.sql
@@ -5,10 +5,10 @@ CREATE PROCEDURE sp_apertura8(
     IN `clave_b` VARCHAR(12), 
     IN `fecha_pol` VARCHAR(11),
     IN `poliza` VARCHAR(4),
-    IN `imp_cap` DECIMAL,
-    IN `imp_eng` DECIMAL,
-    IN `imp_pagos` DECIMAL,
-    IN `imp_sui` DECIMAL,
+    IN `imp_cap` DECIMAL(10,3),
+    IN `imp_eng` DECIMAL(9,3),
+    IN `imp_pagos` DECIMAL(9,2),
+    IN `imp_sui` DECIMAL(9,3),
     IN `id_usuario` INT)
 BEGIN
     insert into mov_edoscta (id_benef,clave_mov,capital, poliza,fecha_pol, clave_b, id_usuario) values (id_benef,'CAP',imp_cap,poliza,fecha_pol, clave_b, id_usuario);
