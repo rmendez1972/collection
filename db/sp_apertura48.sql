766d534219a6ffa5d5fb4d20f8cc98f92dc350d4
Ismael García <igh1@hotmail.com>
2017-10-10 12:52:43 -0500

    Actualización diversos

--- db/sp_apertura48.sql
+++ db/sp_apertura48.sql
@@ -1,4 +1,7 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_apertura48`(
+DROP PROCEDURE IF EXISTS sp_apertura48;
+DELIMITER $$
+
+CREATE  PROCEDURE `sp_apertura48`(
 	IN `id_benef` INT,
 	IN `poliza` VARCHAR(6),
 	IN `clave_b` VARCHAR(12),
@@ -7,10 +10,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_apertura48`(
 	IN `numcontrato` VARCHAR(5),
 	IN `fecha_pol` VARCHAR(11),
 	IN `imp_cap` DECIMAL(10,3),
-	IN `imp_int` DECIMAL(11,3)
-
-
-,
+	IN `imp_int` DECIMAL(11,3),
 	IN `imp_svida` DECIMAL(11,3),
 	IN `imp_admon` DECIMAL(11,3),
 	IN `imp_tit` DECIMAL(11,3),
@@ -18,21 +18,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_apertura48`(
 	IN `imp_sui` DECIMAL(11,3),
 	IN `imp_sin` DECIMAL(11,3),
 	IN `imp_pag` DECIMAL(11,3)
-
-
-
-
-
-
-
-
-
+	
 )
-LANGUAGE SQL
-NOT DETERMINISTIC
-CONTAINS SQL
-SQL SECURITY DEFINER
-COMMENT ''
+
 BEGIN
 insert into mov_edoscta (id_benef,clave_mov,capital, poliza,fecha_mov, clave_b, id_usuario, id_catprog, numcontrato)  
 values (id_benef,'CAP',imp_cap,poliza,fecha_pol, clave_b, id_usuario, id_catprog, numcontrato);
@@ -61,4 +49,5 @@ values (id_benef,'SIN',imp_sin*-1,poliza,fecha_pol, clave_b, id_usuario, id_catp
 insert into mov_edoscta (id_benef,clave_mov,capital, poliza,fecha_mov, clave_b, id_usuario, id_catprog, numcontrato) 
 values (id_benef,'PAG',imp_pag*-1,poliza,fecha_pol, clave_b, id_usuario, id_catprog, numcontrato);
 
-END
+END $$
+DELIMITER ;
